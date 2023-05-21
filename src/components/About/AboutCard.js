75a12050f56168b22309d50cc44b41304cd37971
devkyle4 <devkyle4@gmail.com>
2023-05-21 22:18:06 +0000

    changes in about

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,10 +8,10 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="purple">Evans Kwasi </span>
+            from <span className="purple"> Ghana.</span>
+            <br />I am also known as <span className="purple">Devkyle</span> on
+            the internet.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -24,12 +24,16 @@ function AboutCard() {
               <ImPointRight /> Writing Tech Blogs
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Reading Biographies
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Football/Soccer
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Start making the difference Now<br></br>
+            because Tomorrow will be built on Today!"{" "}
           </p>
           <footer className="blockquote-footer">Evans</footer>
         </blockquote>
